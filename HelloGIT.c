9345f40ed451ff01314e46eb3b52723651b9a6ec
Prasanna <Prasanna@ev.com>
2022-08-04 12:38:21 +0530

    UI Update

--- HelloGIT.c
+++ HelloGIT.c
@@ -1,6 +1,6 @@
 #include<stdio.h>
 int main()
 {
-  printf ("Hello GIT v2");
+  printf ("Hello GIT v3");
   return 0;
 }
--- Namaste.c
+++ Namaste.c
@@ -1,6 +1,6 @@
 #include<stdio.h>
 int main()
 {
-  printf("Namaste GIT v2");
+  printf("Namaste GIT v3");
   return 0;
 }
